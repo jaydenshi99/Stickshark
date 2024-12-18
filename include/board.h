58c5194eec40d330adc3a81714baf22da7c9bc18
Jayden Shi <z5589182@ad.unsw.edu.au>
2024-12-18 22:59:49 +1100

    basic board class setup

--- include/board.h
+++ include/board.h
@@ -1,2 +1,28 @@
 #pragma once
 
+#include <cstdint>
+
+#define WPAWN   0
+#define WBISHOP 1
+#define WKNIGHT 2
+#define WROOK   3
+#define WQUEEN  4
+#define WKING   5
+#define BPAWN   6
+#define BBISHOP 7
+#define BKNIGHT 8
+#define BROOK   9
+#define BQUEEN  10
+#define BKING   11
+
+class Board {
+    public:
+    // Board Data
+    uint64_t pieces[12];
+    bool turn; // true - white | false - black
+
+    // Get methods
+    uint64_t getBoardPositions() const;
+    uint64_t getWhitePositions() const;
+    uint64_t getBlackPositions() const;
+};
---
+++ src/board.cpp
@@ -0,0 +1,15 @@
+#include "../include/board.h"
+
+using namespace std;
+
+uint64_t Board::getBoardPositions() const {
+    return Board::getWhitePositions() | Board::getBlackPositions();
+}
+
+uint64_t Board::getWhitePositions() const {
+    return pieces[WPAWN] | pieces[WBISHOP] | pieces[WKNIGHT] | pieces[WROOK] | pieces[WQUEEN] | pieces[WKING];
+}
+
+uint64_t Board::getBlackPositions() const {
+    return pieces[BPAWN] | pieces[BBISHOP] | pieces[BKNIGHT] | pieces[BROOK] | pieces[BQUEEN] | pieces[BKING];
+}
