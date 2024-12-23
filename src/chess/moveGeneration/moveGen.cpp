604eecc2c402a64c4d98f94480cfcd65fc657f0a
Jayden Shi <z5589182@ad.unsw.edu.au>
2024-12-24 00:32:06 +1100

    diagonal attacks

--- src/chess/moveGeneration/moveGen.cpp
+++ src/chess/moveGeneration/moveGen.cpp
@@ -1,6 +1,9 @@
 #include "moveGen.h"
 #include "../../utility.h"
 
+#define FILEA 0x8080808080808080
+#define FILEH 0x0101010101010101
+
 #define RANK4 0x00FF000000
 #define RANK5 0xFF00000000
 
@@ -9,17 +12,26 @@ using namespace std;
 MoveGen::MoveGen() {};
 
 void MoveGen::generatePawnMoves(Board b) {
-    int pawnIndex = b.turn ? WPAWN : BPAWN;
-    uint64_t doublePushRank = b.turn? RANK4 : RANK5;
+    int pawnBitboard = b.turn ? b.pieceBitboards[WPAWN] : b.pieceBitboards[BPAWN];
+    uint64_t doublePushRank = b.turn ? RANK4 : RANK5;
     uint64_t blockers = b.getBlockers();
+    uint64_t enemy = b.turn ? b.getBlackPositions() : b.getWhitePositions();
 
     uint64_t singlePushes = b.turn 
-        ? (b.pieceBitboards[pawnIndex] << 8) & ~blockers
-        : (b.pieceBitboards[pawnIndex] >> 8) & ~blockers;
+        ? (pawnBitboard << 8) & ~blockers
+        : (pawnBitboard >> 8) & ~blockers;
 
     uint64_t doublePushes = b.turn 
         ? (singlePushes << 8) & ~blockers & doublePushRank
         : (singlePushes >> 8) & ~blockers & doublePushRank;
+
+    uint64_t leftDiagonalAttacks = b.turn
+        ? (pawnBitboard << 9) & enemy & ~FILEH
+        : (pawnBitboard >> 7) & enemy & ~FILEH;
+
+    uint64_t rightDiagonalAttacks = b.turn
+        ? (pawnBitboard << 7) & enemy & ~FILEA
+        : (pawnBitboard >> 9) & enemy & ~FILEA;
     
     int singlePushOffset = b.turn ? -8 : 8;
     while (singlePushes) {
@@ -31,6 +43,18 @@ void MoveGen::generatePawnMoves(Board b) {
     while (doublePushes) {
         int target = popLSB(&doublePushes);
         moves.push_back(Move(target + doublePushOffset, target, PAWNTWOFORWARD));
+    }
+
+    int leftDiagonalAttackOffset = b.turn ? -9 : 7;
+    while (leftDiagonalAttacks) {
+        int target = popLSB(&leftDiagonalAttacks);
+        moves.push_back(Move(target + leftDiagonalAttackOffset, target, NONE));
+    }
+
+    int rightDiagonalAttackOffset = b.turn ? -7 : 9;
+    while (rightDiagonalAttacks) {
+        int target = popLSB(&rightDiagonalAttacks);
+        moves.push_back(Move(target + rightDiagonalAttackOffset, target, NONE));
     }
 }
 
