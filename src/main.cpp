64eb3a1c6e4a75a5c94076e615f29053d1487d43
Jayden Shi <z5589182@ad.unsw.edu.au>
2024-12-25 13:38:49 +1100

    update main for testing

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main () {
     computeAllTables();
 
     for (int i = 0; i < 64; i++) {
-        cout << bitset<64>(rookMagics[i]) << endl;
+        cout << rookMagics[i] << endl;
     }
     
     return 0;
