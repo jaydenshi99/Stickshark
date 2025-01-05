fd53c09ba3e4c8a28a1e9ca144d7cc1635b2929e
Jayden Shi <z5589182@ad.unsw.edu.au>
2025-01-05 23:26:31 +1100

    add readme.md

--- src/engine/evalConstants.cpp
+++ src/engine/evalConstants.cpp
@@ -6,8 +6,8 @@ const int materialEvaluations[NUM_PIECES] = {
     -100, -330, -320, -500, -900, -20000
 };
 
-// Absolute value version of material evaluations. King weighted heavily to move back illegal moves
+// Absolute value version of material evaluations. King weighted heavily to predict king safety
 const int moveScoreMaterialEvaluations[NUM_PIECES] = {
-    100, 330, 320, 500, 900, 20000, 
-    100, 330, 320, 500, 900, 20000
+    100, 330, 320, 500, 900, 900, 
+    100, 330, 320, 500, 900, 900
 };
