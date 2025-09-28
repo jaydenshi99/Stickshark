d9331d121de40651d4042a257dcf44aa259ea5e1
Jayden <jayden.shi@student.unsw.edu.au>
2025-09-28 12:08:47 +1000

    add endgame phase calculation

--- src/engine/evaluation.cpp
+++ src/engine/evaluation.cpp
@@ -9,6 +9,7 @@ int staticEvaluation(const Board& board) {
         return 0; 
     }
     
+    // Material eval
     int eval = 0;
     for (int i = 0; i < 12; i++) {
         eval += popcount(board.pieceBitboards[i]) * materialEvaluations[i];
@@ -17,4 +18,16 @@ int staticEvaluation(const Board& board) {
     eval += board.pieceSquareEval;
 
     return eval;
+}
+
+int getEndgamePhase(const Board& board) {
+    int phase = 0;
+    phase += popcount(board.pieceBitboards[WPAWN] | board.pieceBitboards[BPAWN]) * PHASE_P;
+    phase += popcount(board.pieceBitboards[WKNIGHT] | board.pieceBitboards[BKNIGHT]) * PHASE_N;
+    phase += popcount(board.pieceBitboards[WBISHOP] | board.pieceBitboards[BBISHOP]) * PHASE_B;
+    phase += popcount(board.pieceBitboards[WROOK] | board.pieceBitboards[BROOK]) * PHASE_R;
+    phase += popcount(board.pieceBitboards[WQUEEN] | board.pieceBitboards[BQUEEN]) * PHASE_Q;
+
+    phase = min(phase, MAX_PHASE);
+    return phase;
 }
--- src/engine/evaluation.h
+++ src/engine/evaluation.h
@@ -4,6 +4,9 @@
 #include "evalConstants.h"
 #include "../bit.h"
 
+static const int PHASE_P=0, PHASE_N=1, PHASE_B=1, PHASE_R=2, PHASE_Q=4;
+static const int MAX_PHASE = 24;
+
 int staticEvaluation(const Board& board);
 
-bool isEndgame(const Board& board);
+int getEndgamePhase(const Board& board);
